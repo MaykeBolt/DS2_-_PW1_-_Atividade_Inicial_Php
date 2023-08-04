3c6e3b780c378349dbf703f8e4a7f12f92541ed5
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2023-08-03 22:22:10 -0300

    Echo com um h1 e a letra da musica do Sonic R.

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "<h1>Sonic R Soundtrack: Work It Out</h1>
+We all need to work together to build a better life
+<br>
+To make it what we feel, we have to make it real
+<br>
+We all need to remember
+<br>
+That nothing in this world comes for free
+<br>
+We have to make it what we want
+<br>
+Just you and me and everybody
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+Work it out, work it out, talk about it
+<br>
+Work it out, work it out, make it happen
+<br>
+Let's work it out, work it out
+<br>
+<br>
+We've got to make it real
+<br>
+We've got to make it real
+<br>
+We've got to make it real
+<br>
+We've got to make it real
+<br>
+<br>
+We all know that life gets harder, it's gettin' harder day by day
+<br>
+And we can show that we are tryin'
+<br>
+We're makin' sure that everyone can say
+<br>
+''You're the one I can depend on'', no one else can put it right
+<br>
+I'm someone you can rely on
+<br>
+'Cause we're not givin' up without a fight
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+Work it out, work it out, talk about it
+<br>
+Work it out, work it out, make it happen
+<br>
+Let's work it out, work it out
+<br>
+<br>
+We've got to make it real
+<br>
+We've got to make it real
+<br>
+We've got to make it real
+<br>
+We've got to make it real
+<br>
+<br>
+Yeah
+<br>
+Oh-oh, oh-oh
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+Work it out, work it out, talk about it
+<br>
+Work it out, work it out, make it happen
+<br>
+Let's work it out, work it out
+<br>
+We've got to make it real
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, talk about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, make it happen
+<br>
+(We've got to make it real)
+<br>
+Let's work it out, work it out
+<br>
+We've got to make it real
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, talk about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, make it happen
+<br>
+(We've got to make it real)
+<br>
+Let's work it out, work it out
+<br>
+We've got to make it real
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+(We've got to make it real) (Think about it)
+<br>
+Work it out, work it out, talk about it
+<br>
+(We've got to make it real) (Talk about it)
+<br>
+Work it out, work it out, make it happen
+<br>
+(We've got to make it real) (Make it happen)
+<br>
+Let's work it out, work it out
+<br>
+We've got to make it real
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, talk about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, make it happen
+<br>
+(We've got to make it real)
+<br>
+Let's work it out, work it out
+<br>
+We've got to make it real (real, yeah)
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, talk about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, make it happen
+<br>
+(We've got to make it real)
+<br>
+Let's work it out, work it out
+<br>
+We've got to make it real
+<br>
+<br>
+Work it out, work it out, think about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, talk about it
+<br>
+(We've got to make it real)
+<br>
+Work it out, work it out, make it happen
+<br>
+(We've got to make it real)
+<br>
+Let's work it out, work it out
+<br>
+We've got to make it real
+<br>
+We've got to make it real";
+
+?>
+</body>
+</html>
